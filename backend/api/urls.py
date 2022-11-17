35b8c272668b3e6371d38402207697620ee61c34
Sam-Tech-Ideas <agyemangsamuel235@gmail.com>
2022-11-17 02:33:11 +0000

    Using UpdateAPIView

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
     path('todos/', views.TodoListCreate.as_view()),
     path('todos/<int:pk>', views.TodoRetrieveUpdateDestroy.as_view()),
-    
+    path('todos/<int:pk>/complete', views.TodoToggleComplete.as_view()),
+
     
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework import generics, permissions
-from .serializers import TodoSerializer
+from .serializers import TodoSerializer,TodoToggleCompleteSerializer
 from todo.models import Todo
 
 # Create your views here.
@@ -40,5 +40,17 @@ class TodoRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     def get_queryset(self):
         user = self.request.user
         return Todo.objects.filter(user=user)
-        
+
+class TodoToggleComplete(generics.UpdateAPIView):
+    serializer_class = TodoToggleCompleteSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user 
+        return Todo.objects.filter(user=user)
+
+
+    def perform_update(self,serializer):
+        serializer.instance.completed = not(serializer.instance.completed)
+        serializer.save()
 
